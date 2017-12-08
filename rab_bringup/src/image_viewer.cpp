0cf80ae05bfb97da39fb66637ffdfb8b5df773e9
shibuya-bitvalley <ryu.j.shibu@gmail.com>
2017-12-08 11:02:40 +0900

    [add]image_viewer(not complete)

---
+++ rab_bringup/src/image_viewer.cpp
@@ -0,0 +1,25 @@
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <cmath>
+
+void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg){
+   double orientation_z = 0;
+   double orientation_w = 0;
+   double th0 = 0;
+   th0 = 2 * asin(msg->pose.pose.orientation.z / 2);
+   double th1 = 0;
+   th1 = 2 * acos(msg->pose.pose.orientation.w / 2);
+   ROS_INFO("Seq: [%d]", msg->header.seq);
+   ROS_INFO("Position-> x: [%lf], y: [%lf]", msg->pose.pose.position.x, msg->pose.pose.position.y);
+   ROS_INFO("Orientation-> th0: [%lf], th1: [%lf]", th0 * 180 / 3.14, th1 * 180 / 3.14);
+}
+
+
+int main(int argc, char **argv){
+   ros::init(argc, argv, "image_viewer");
+   ros::NodeHandle n;
+   ros::Subscriber sub;
+   sub = n.subscribe("/diff_drive_controller/odom", 1000, chatterCallback);
+   
+   ros::spin();
+}
