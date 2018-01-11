8ad5e72c2afb4367e5547c931944626ad596c9dc
shibuya-bitvalley <ryu.j.shibu@gmail.com>
2018-01-11 20:32:18 +0900

    [mod]条件分岐（座標ではない）

--- rab_bringup/src/image_viewer.cpp
+++ rab_bringup/src/image_viewer.cpp
@@ -42,28 +42,49 @@ int main(int argc, char **argv){
    ros::init(argc, argv, "image_viewer");
    ros::NodeHandle n("~");
    ros::Subscriber sub;
+   //subscribe topicの設定
    std::string odom_topic_;
    n.param<std::string>("odom_topic", odom_topic_, "/diff_drive_controller/odom"); 
    sub = n.subscribe(odom_topic_, 1000, chatterCallback);
    
+   //画像読み込み１枚目
    cv::Mat first_img;
    std::string first_path_;
    n.param<std::string>("first_path", first_path_, ros::package::getPath("rab_bringup") + "/picture/sample.JPG");
-   if(n.getParam("first_path", first_path_)){
+   /*
+    if(n.getParam("first_path", first_path_)){
       ROS_INFO("Got Param: %s", first_path_.c_str());
    }else{
       n.setParam("first_path", first_path_.c_str());
       ROS_WARN("Set 'first_path' default!");
-   }
-   
+   }*/
    first_img = cv::imread(first_path_, 1);
+
+   //2枚目
+   cv::Mat second_img;
+   std::string second_path_;
+   n.param<std::string>("second_path", second_path_, ros::package::getPath("rab_bringup") + "/picture/ubuntu-logo.png");
+   second_img = cv::imread(second_path_, 1);
+   
    // 画像が読み込まれなかったら終了
-   if(first_img.empty()) return -1;
+   if(first_img.empty() || second_img.empty()) return -1;
       
    // 画像表示
    cv::namedWindow("Image", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
-   cv::imshow("Image", first_img);
-   cv::waitKey(0);
+
+   for(int i = 0; i < 10; i++) {
+      if(i <= 4){
+	 cv::imshow("Image", first_img);
+	 cv::waitKey(1);
+	 ROS_WARN("場合分け１");
+      }else{
+	 cv::imshow("Image", second_img);
+	 cv::waitKey(1);
+	 ROS_ERROR("場合分け２");
+      }
+      ROS_INFO("time = %d", i);
+      sleep(1);
+   }
    ros::spin();
    return 0;
 }
