eb065f3db542777f7138806950a2dc603adee092
shibuya-bitvalley <ryu.j.shibu@gmail.com>
2018-01-11 14:40:03 +0900

    [mod]param

--- rab_bringup/src/image_viewer.cpp
+++ rab_bringup/src/image_viewer.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 #include <ros/package.h>
-//#include <nav_msgs/Odometry.h>
-//#include <cmath>
+#include <nav_msgs/Odometry.h>
+#include <cmath>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
@@ -12,6 +12,7 @@ class odom{
  private:
    void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg);
 };
+*/
    
 void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg){
    double orientation_z = 0;
@@ -25,7 +26,7 @@ void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg){
    ROS_INFO("Orientation-> th0: [%lf], th1: [%lf]", th0 * 180 / 3.14, th1 * 180 / 3.14);
 }
 
-
+/*
 //画像表示を行うためのフラグをいじる関数
 int changeflag(const nav_msgs::Odometry::ConstPtr& msg){
    double orientation_z = 0;
@@ -40,28 +41,29 @@ int changeflag(const nav_msgs::Odometry::ConstPtr& msg){
 int main(int argc, char **argv){
    ros::init(argc, argv, "image_viewer");
    ros::NodeHandle n("~");
-   //ros::Subscriber sub;
-   //sub = n.subscribe("/odom", 1000, chatterCallback);
+   ros::Subscriber sub;
+   std::string odom_topic_;
+   n.param<std::string>("odom_topic", odom_topic_, "/diff_drive_controller/odom"); 
+   sub = n.subscribe(odom_topic_, 1000, chatterCallback);
    
    cv::Mat first_img;
-   std::string first_path;
-   std::string image_path_;
-   n.param<std::string>("image_path", first_path, ros::package::getPath("rab_bringup") + "/picture/sample.JPG");
-   if(n.getParam("image_path", image_path_)){
-      ROS_INFO("Got Param: %s", image_path_.c_str());
+   std::string first_path_;
+   n.param<std::string>("first_path", first_path_, ros::package::getPath("rab_bringup") + "/picture/sample.JPG");
+   if(n.getParam("first_path", first_path_)){
+      ROS_INFO("Got Param: %s", first_path_.c_str());
    }else{
-      n.setParam("image_path", image_path_.c_str());
-      ROS_WARN("Set 'image_path' default!");
+      n.setParam("first_path", first_path_.c_str());
+      ROS_WARN("Set 'first_path' default!");
    }
-   //ROS_INFO("first path = %s", first_path.c_str());
-   first_img = cv::imread(first_path, 1);
-   // 画像が読み込まれなかったら終了
-   if(first_img.empty()) return -1;
    
+   first_img = cv::imread(first_path_, 1);
+   // 画像が読み込まれなかったら終了
+   if(first_img.empty()) ROS_ERROR("can't load image");
+      
    // 画像表示
    cv::namedWindow("Image", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
    cv::imshow("Image", first_img);
-   cv::waitKey();
+   cv::waitKey(0);
    ros::spin();
    return 0;
 }
