39c214357ad2b96c3132e2ced26ad650aa7b4b73
shibuya-bitvalley <ryu.j.shibu@gmail.com>
2018-01-17 21:59:03 +0900

    [mod]画像サイズ変更

--- opencv_practice/test.cpp
+++ opencv_practice/test.cpp
@@ -3,9 +3,11 @@
 #include "unistd.h"
 #include <iostream>
 using namespace std;
+using namespace cv;
 
 int main(void){
    cv::Mat src_img;
+   cv::Mat first;
    src_img = cv::imread("picture/ubuntu-logo.png", 1);
    
    cv::Mat hoge;
@@ -20,18 +22,18 @@ int main(void){
    // 結果画像表示
    cv::namedWindow("Image", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
 
-   for(int i = 0; i < 10; i++){
-      if(i <= 4){
-	 cv::imshow("Image", src_img);
-	 cv::waitKey(1);
-	 //break;
-      }else{
-	 cv::imshow("Image", hoge);
-	 cv::waitKey(1);
-	 //break;
-      }
-      cout << "time = "<< i << endl;
-      sleep(1);
-   }
+   //for(int i = 0; i < 10; i++){
+      //if(i <= 4){
+	 cv::resize(src_img, first, Size(), 0.5, 0.5);
+         cv::imshow("Image", first);
+         //cv::imshow("Image", src_img);
+	 cv::waitKey(0);
+      //}else{
+	 //cv::imshow("Image", hoge);
+	 //cv::waitKey(1);
+      //}
+      //cout << "time = "<< i << endl;
+      //sleep(1);
+   //}
    return 0;
 }
