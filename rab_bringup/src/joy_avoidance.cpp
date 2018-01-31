0954581d86f3987e503b31789d9e15974cfab603
shibuya-bitvalley <ryu.j.shibu@gmail.com>
2018-01-31 18:01:44 +0900

    [mod]衝突回避（遠隔用）

--- rab_bringup/src/joy_avoidance.cpp
+++ rab_bringup/src/joy_avoidance.cpp
@@ -26,7 +26,9 @@ class JoyAvoidance{
    std::string pub_cmdvel_topic_;
    
    // declaration of menber variable
-   double range_min;
+   double front;
+   double right;
+   double left;
    int flag;
 };
 
@@ -36,30 +38,39 @@ JoyAvoidance::JoyAvoidance(ros::NodeHandle &nh){
    n.param<std::string>("scan_topic", scan_topic_, "\"/base_scan\"");
    n.param<std::string>("pub_cmdvel_topic", pub_cmdvel_topic_, "\"/cmd_vel\"");
    // Subscriber
-   sub_scan = nh.subscribe(scan_topic_, 10, &JoyAvoidance::sensorCallback, this);
-   sub_vel = nh.subscribe(sub_cmdvel_topic_, 1, &JoyAvoidance::cmdvelCallback, this);
+   sub_scan = nh.subscribe("/base_scan", 10, &JoyAvoidance::sensorCallback, this);
+   sub_vel = nh.subscribe("/cmd_vel_old", 1, &JoyAvoidance::cmdvelCallback, this);
    // Publisher
-   pub_vel = nh.advertise<geometry_msgs::Twist>("pub_cmdvel_topic", 100);
+   pub_vel = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 100);
 }
 
-void sensorCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
+void JoyAvoidance::sensorCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
    // 位置に応じてフラグ変更
-   range_min = scan.range_min;
-   ROS_INFO("min range = %lf", range_min);
-   
-   if(range_min < 1.0){
+   this->front = scan->ranges.size() / 2; // 正面方向
+   this->right = scan->ranges.size() / 4; // 右90度方向
+   this->left = scan->ranges.size() * 3 / 4; // 左90度方向
+   ROS_INFO("front range = %lf", scan->ranges[front]);
+   ROS_INFO("right limit range = %lf", scan->ranges[right]);
+   ROS_INFO("left limit range = %lf", scan->ranges[left]);
+   if(scan->ranges[front] < 2.0 || scan->ranges[right] < 1.0 || scan->ranges[left] < 1.0){
       flag = 1;
    }else{
       flag = 0;
    }
+   ROS_ERROR("flag : %d", flag);
 }
 
-void cmdvelCallback(const geometry_msgs::Twist::ConstPtr& vel){
+void JoyAvoidance::cmdvelCallback(const geometry_msgs::Twist::ConstPtr& vel){
    // フラグに応じてcmd_velの値を調整しをpublish
    geometry_msgs::Twist cmd_vel;
-   if(flag == 1 && vel.linear.x > 0.0){
+   if(flag == 1 && vel->linear.x > 0.0){
       cmd_vel.linear.x = 0.0;
+   }else{
+      cmd_vel.linear.x = vel->linear.x;
    }
+   cmd_vel.linear.z = vel->linear.z;
+   cmd_vel.angular.z = vel->angular.z;
+   cmd_vel.linear.y = 0.0;
    pub_vel.publish(cmd_vel);
 }
 
@@ -69,21 +80,3 @@ int main(int argc, char** argv){
    JoyAvoidance joy_avoidance(n);
    ros::spin();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
